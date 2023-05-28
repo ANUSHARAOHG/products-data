647509169bb53cdf6afd23372f8f5d33907673ef
Anusha Rao H G <83913895+ANUSHARAOHG@users.noreply.github.com>
2023-05-28 18:26:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ http.onload = function(){
 		for(let item of products){
 			output += `
 				<div class="product">
-					<img src="${item.image}" alt="${item.description}">
-					<p class="title">${item.title}</p>
-					<p class="description">${item.description}</p>
-					<p class="price">
-						<span>${item.price}</span>
-						<span>&euro;</span>
-					</p>
+					
+					<p class="title">${item.MACHINE}</p>
+// 					<p class="description">${item.description}</p>
+// 					<p class="price">
+// 						<span>${item.price}</span>
+// 						<span>&euro;</span>
+// 					</p>
 					<p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
 				</div>
 			`;

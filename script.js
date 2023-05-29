a6ed145e3ac79e664acd23762a5d50bf16dd8ec3
Anusha Rao H G <83913895+ANUSHARAOHG@users.noreply.github.com>
2023-05-29 13:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,27 @@
-/*
-	We start our code with an ajax request to fetch the data
-	from the json file.
-*/
-// First i create a new xmlhttp-request object.
-let http = new XMLHttpRequest();
-// the variable http holds now all methods and properties of the objct.
-
-//  next i prepare the request with the open() method.
-http.open('get', 'products.json', true);
-// the first argument sets the http method.
-// in the second argument we pass the file where our data lives.
-// and last the keyword true, sets the request to be async.
-
-// next i will send the request.
-http.send();
-
-// Now i have to catch the response.
-// i will check the onload eventlistener.
-http.onload = function(){
-	// Inside the function i need to check the readystate and status properties.
-	if(this.readyState == 4 && this.status == 200){
-		// if we have a successful response, i have to parse the json data
-		// and convert them to a javascript array.
-		let products = JSON.parse(this.responseText);
-
-		// next i need an empty variable to add the incoming data.
-		let output = "";
-
-		// now i have to loop trough the products, and in every iteration
-		// i add an html template to the output variable.
-		for(let item of products){
-			output += `
-				<div class="product">
-				
-					<div class="row">
-				
-						<div class="col-9">
-							 <h3>Machine: ${item.MACHINE}</h3>
-						</div>
-						 <div class="col-3">
-						   <button class="btn btn-success">${item.STATUS}</button>
-						 </div>
-					 </div>
-					
-				      <p>Performance: ${item.PERFORMANCE}</p>
-				     
-				      <p>Running Since: ${item['RUNNING SINCE']}</p>
-				      <p>Stopped Since: ${item['STOPPED SINCE']}</p>
-				      <p>Parts Produced: ${item['PARTS PRODUCED']}</p>
-					
-					
-				</div>
-				
-				
-			`;
-		}
-		/* and last i target the products container and add the data that the
-		output variable holds. */
-		document.querySelector(".products").innerHTML = output;
-	}
-		
-		
-} 
+$(document).ready(function() {
+    // AJAX request to fetch the machine data
+    $.ajax({
+      url: 'machines.json', // Replace 'machines.json' with the path to your JSON file
+      dataType: 'json',
+      success: function(data) {
+        // Iterate over each machine object and create a card
+        $.each(data, function(index, machine) {
+          var cardHtml = `
+            <div class="product">
+              <h3>Machine: ${machine.MACHINE}</h3>
+              <p>Performance: ${machine.PERFORMANCE}</p>
+              <p>Status: ${machine.STATUS}</p>
+              <p>Running Since: ${machine['RUNNING SINCE']}</p>
+              <p>Stopped Since: ${machine['STOPPED SINCE']}</p>
+              <p>Parts Produced: ${machine['PARTS PRODUCED']}</p>
+            </div>
+          `;
+          $('#machine-container').append(cardHtml);
+        });
+      },
+      error: function() {
+        alert('Failed to fetch machine data.');
+      }
+    });
+  });
+  
